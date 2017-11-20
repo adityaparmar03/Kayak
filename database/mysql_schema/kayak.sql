9f30f0481738016bb753bb6624afe071f6086c6a
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-11-19 18:21:44 -0800

    Modifying the schema

--- database/mysql_schema/kayak.sql
+++ database/mysql_schema/kayak.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE  IF NOT EXISTS `kayak` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `kayak`;
-
 --
 -- Table structure for table `BILLING`
 --
@@ -8,28 +7,52 @@ USE `kayak`;
 DROP TABLE IF EXISTS `BILLING`;
 
 CREATE TABLE `BILLING` (
-  `billing_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `billing_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(90) NOT NULL,
   `booking_type` enum('CAR','FLIGHT','HOTEL') NOT NULL,
-  `booking_date` varchar(45) DEFAULT NULL,
+  `booking_date` datetime DEFAULT NULL,
   `billing_date` datetime NOT NULL,
   `billing_amount` int(11) NOT NULL COMMENT 'Billing Amount in US Dollars',
   `source` varchar(45) DEFAULT NULL,
   `destination` varchar(45) DEFAULT NULL,
   `booking_start_date` datetime DEFAULT NULL,
   `booking_end_date` datetime DEFAULT NULL,
-  `car_miles_used` int(11) DEFAULT NULL,
   `person_count` int(11) DEFAULT NULL COMMENT 'Not applicable for car bookings',
-  PRIMARY KEY (`billing_id`),
-  KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`billing_id`,`user_email`),
+  KEY `user_id_idx` (`user_email`),
+  CONSTRAINT `user_email` FOREIGN KEY (`user_email`) REFERENCES `USER` (`email`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `BILLING` WRITE;
+
+UNLOCK TABLES;
+
+--
+-- Table structure for table `CART`
+--
+
+DROP TABLE IF EXISTS `CART`;
+
+CREATE TABLE `CART` (
+  `cart_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(90) NOT NULL,
+  `booking_type` enum('CAR','FLIGHT','HOTEL') DEFAULT NULL,
+  `billing_amount` int(11) DEFAULT NULL,
+  `source` varchar(45) DEFAULT NULL,
+  `destination` varchar(45) DEFAULT NULL,
+  `booking_start_date` datetime DEFAULT NULL,
+  `booking_end_date` datetime DEFAULT NULL,
+  `person_count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cart_id`,`user_email`),
+  KEY `user_email_cart_idx` (`user_email`),
+  CONSTRAINT `user_email_cart` FOREIGN KEY (`user_email`) REFERENCES `USER` (`email`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `BILLING`
+-- Dumping data for table `CART`
 --
 
-LOCK TABLES `BILLING` WRITE;
+LOCK TABLES `CART` WRITE;
 
 UNLOCK TABLES;
 
@@ -40,29 +63,25 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `USER`;
 
 CREATE TABLE `USER` (
-  `user_id` int(11) NOT NULL,
+  `email` varchar(90) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `user_role` enum('USER','ADMIN') DEFAULT NULL,
-  `address` varchar(90) NOT NULL,
+  `street_address` varchar(90) NOT NULL,
   `city` varchar(45) NOT NULL,
   `state` varchar(45) NOT NULL,
   `zip_code` int(11) NOT NULL,
-  `phone` int(11) NOT NULL,
-  `email` varchar(45) NOT NULL,
+  `phone` varchar(45) NOT NULL,
   `profile_image_path` varchar(90) DEFAULT NULL,
   `credit_card_holder_name` varchar(45) DEFAULT NULL,
   `credit_card_number` varchar(45) DEFAULT NULL,
   `credit_card_valid_from` date DEFAULT NULL,
   `credit_card_valid_till` date DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
+  PRIMARY KEY (`email`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
---
--- Dumping data for table `USER`
---
 
 LOCK TABLES `USER` WRITE;
 
@@ -75,14 +94,13 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `USER_ACTIVITY`;
 
 CREATE TABLE `USER_ACTIVITY` (
-  `activity_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `activity_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(90) NOT NULL,
   `activity_type` enum('CLICK') NOT NULL DEFAULT 'CLICK',
   `activity_target_page` enum('FLIGHT_BOOK','CAR_BOOK','HOTEL_BOOK') NOT NULL,
-  PRIMARY KEY (`activity_id`),
-  KEY `user_id_idx` (`user_id`),
-  KEY `user_id_act_idx` (`user_id`),
-  CONSTRAINT `user_id_act` FOREIGN KEY (`user_id`) REFERENCES `USER` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`activity_id`,`user_email`),
+  KEY `user_id_act_idx` (`user_email`),
+  CONSTRAINT `user_email_act` FOREIGN KEY (`user_email`) REFERENCES `USER` (`email`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
