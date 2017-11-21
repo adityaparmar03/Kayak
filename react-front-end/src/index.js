52268000a6768785046d32a71f7cc40fdf23a23d
Aditya Parmar <parmar415@gmail.com>
2017-11-20 23:28:23 -0800

    Routing Implemented

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import Home from "./components/home";
 import { createStore } from 'redux';
 import reducer from './reducers/index';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 
 const store = createStore(
     reducer,
@@ -14,7 +17,22 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+         <Router>
+         <MuiThemeProvider> 
+            <div>
+       
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/flight" component={Home}/>
+                <Route exact path="/hotel" component={Home}/>
+                <Route exact path="/car" component={Home}/>
+                <Route exact path="/flightlist" component={Home}/>
+                <Route exact path="/hotellist" component={Home}/>
+                <Route exact path="/carlist" component={Home}/>
+       
+        
+            </div>
+            </MuiThemeProvider>
+     </Router>
     </Provider>
     ,
     document.getElementById('root')
