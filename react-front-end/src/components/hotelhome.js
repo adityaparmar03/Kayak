51738547b4df1ace2b35d26d3a7ddc54d55a244d
Aditya Parmar <parmar415@gmail.com>
2017-11-22 22:45:40 -0800

    Hotel Home Completed.

---
+++ react-front-end/src/components/hotelhome.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+//import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import FlatButton from 'material-ui/FlatButton';
+
+
+import '../css/home.css'
+//Compnents
+import Nav from './nav'
+import Flightsearch from './flightsearch'
+import Hotelsearch from './hotelsearch'
+import Carsearch from './carsearch'
+class Hotelhome extends Component {
+
+  
+    
+     
+    componentWillMount(){
+        //injectTapEventPlugin()
+    }
+   
+    handleSearch(){
+    
+    }
+     
+    render(){
+        return(
+            <div  style={{ backgroundImage: 'url(' + require('../image/hotelbg.jpg') + ')', height: '600px'}}>
+            
+       <Nav/>
+
+       
+
+                <p style={{color:'white',
+                fontWeight:'700',fontSize:'25',
+                marginTop:'5%',marginLeft:"30%",
+                marginRight:"30%"}}>Search hundreds of travel sites at once.</p>
+
+                <div style={{marginLeft:'25%',marginRight:'25%',marginTop:"5%"}}>        
+                <ul className="nav nav-tabs nav-justified" role="tablist">
+   
+                    <li className="nav-item">
+                        <a className="nav-link" data-toggle="tab" href="#flight" role="tab"><i className="fa fa-plane"></i> <b> FLIGHTS</b></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" data-toggle="tab" href="#hotel" role="tab"><i className="fa fa-hotel"></i><b>  HOTELS</b></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" data-toggle="tab" href="#car" role="tab"><i className="fa fa-car"></i><b> CARS</b></a>
+                    </li>
+                </ul>
+                </div>
+                <div className="tab-content">
+
+                    <div className="tab-pane fade" id="flight" role="tabpanel" 
+                    style={{paddingTop:"8%",marginTop:'-9%'}}>
+                    <Flightsearch/>
+                    </div>
+
+                    <div className="tab-pane fade in show active" id="hotel" role="tabpanel"  
+                    style={{paddingTop:"8%",marginTop:'-9%'}}>>
+                    <Hotelsearch/>    
+                    </div>
+                
+                    <div className="tab-pane fade" id="car" role="tabpanel"  
+                    style={{paddingTop:"8%",marginTop:'-9%'}}>>
+                    <Carsearch/>    
+                    </div>
+
+                    
+                
+                </div>
+
+
+
+
+
+
+            </div>
+
+        )
+    }
+}
+
+export default Hotelhome;
