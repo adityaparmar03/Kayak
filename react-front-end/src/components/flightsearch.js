e89130a918236429cbda3b2472414f134252f877
Aditya Parmar <parmar415@gmail.com>
2017-11-22 22:44:30 -0800

    Complete Flight Search

--- react-front-end/src/components/flightsearch.js
+++ react-front-end/src/components/flightsearch.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import AutoComplete from 'material-ui/AutoComplete';
 import DatePicker from 'material-ui/DatePicker';
 import TextField from 'material-ui/TextField';
+import moment from 'moment'
 
 class Flightsearch extends Component {
 
@@ -13,40 +14,104 @@ class Flightsearch extends Component {
     }
     state = {
 
+        //textbox values
+        from:"",
+        to:"",
+        startdate:"",
+        enddate:"",
+        
         // UI State
-        airportlist: [],
+        fromsuggestion: [],
+        tosuggestion: [],
         travelerpopup:false,
         traveler:1,
         class:"Economy"
       };
-      handleUpdateInput = (value) => {
+       handleSubmit(){
+        
+        var startdate =moment(this.state.startdate).month()+"/"+
+        moment(this.state.startdate).date()+"/"+
+        moment(this.state.startdate).year()
+        var enddate =moment(this.state.enddate).month()+"/"+
+        moment(this.state.enddate).date()+"/"+
+        moment(this.state.enddate).year()
+
+        console.log("From=>"+this.state.from)
+        console.log("To=>"+this.state.to)
+        console.log("Stat date=>"+startdate)
+        console.log("Return Date=>"+enddate)
+        console.log("Travelers =>"+this.state.traveler)
+        console.log("class =>"+this.state.class)
+
+        // call Api for search here......
+        //API CALL
+
+      }
+
+
+      handleUpdateFromInput = (value,textbox) => {
+       
+        this.setState({"from":value})  
+           
         var term = value.toUpperCase();
         var API = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=NKHaRIuzMxh8bfMSPnKPt3UGrHjDx9AV&term="+term+"&country=US";
-        console.log("API"+API)
-        fetch(API, {method: 'GET', headers: {
+        //var API = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=s&types=(cities)&key=AIzaSyC6yOY5y7Y8pAehpj8khSIIDcjsulHvuFs";
+        
+        fetch(API, {method: 'GET',headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+               
             },
            })
             .then((response) => response.json())
 		    .then((json) => {
-                var airportlist = json.map((item,index)=>item.label)
+                var fromsuggestion = json.map((item,index)=>item.label)
                 this.setState({
-                    airportlist: airportlist
+                    fromsuggestion: fromsuggestion
                   });
 		});
         
       }; 
+      handleUpdateToInput = (value,textbox) => {
+        
+         this.setState({"to":value})  
+            
+         var term = value.toUpperCase();
+        //var API = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=NKHaRIuzMxh8bfMSPnKPt3UGrHjDx9AV&term="+term+"&country=US";
+        var API = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=san&types=(cities)&key=AIzaSyC6yOY5y7Y8pAehpj8khSIIDcjsulHvuFs";
+         fetch(API, {method: 'GET', headers: {
+                 'Accept': 'application/json',
+                 'Content-Type': 'application/json',
+                 'Access-Control-Allow-Origin': "*",
+                 'Access-Control-Allow-Methods':'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+                 
+                 'Access-Control-Allow-Credentials':true
+             },
+            })
+             .then((response) => response.json())
+             .then((json) => {
+                var tosuggestion = json.map((item,index)=>item.description)
+                console.log(tosuggestion)
+                this.setState({
+                    tosuggestion: tosuggestion
+                  });
+         });
+         
+       };
     
     
-    getTraveler(){
-
-    }  
+    handleStartDate(event, date){
+        this.setState({startdate: date})
+    }
+    handleEndDate(event, date){
+        this.setState({enddate: date})
+    }
       
     handlepopup(){
-        this.setState({travellerpopup:!this.state.travellerpopup})
+        this.setState({travelerpopup:!this.state.travelerpopup})
     } 
     changetraveler(op){
+        console.log(this.state)
         if(op=="+"){
             this.setState({traveler:this.state.traveler+1})
         }else{
@@ -59,7 +124,7 @@ class Flightsearch extends Component {
         this.setState({class:cl})
     }
     displaypopup(){
-        if(this.state.travellerpopup){
+        if(this.state.travelerpopup){
             return <div style={{marginTop:"-6%",minWidth:"300px",
             marginLeft:"60%",marginRight:"10%",borderRadius:"0",zIndex:"2"}} className="card">
                 <div className="card-body"> 
@@ -105,10 +170,12 @@ class Flightsearch extends Component {
             
             <AutoComplete
                     hintText="From"
-                    dataSource={this.state.airportlist}
-                    onUpdateInput={this.handleUpdateInput}
+                    dataSource={this.state.fromsuggestion}
+                    onUpdateInput={this.handleUpdateFromInput}
                     floatingLabelText="From"
                     maxSearchResults={5}
+                    underlineShow={false}
+                    
             />
        
             </div>
@@ -116,33 +183,41 @@ class Flightsearch extends Component {
             <div style={{backgroundColor:'white'}}>
             <AutoComplete
                     hintText="To"
-                    dataSource={this.state.airportlist}
-                    onUpdateInput={this.handleUpdateInput}
+                    dataSource={this.state.tosuggestion}
+                    onUpdateInput={this.handleUpdateToInput}
                     floatingLabelText="To"
                     maxSearchResults={5}
+                    underlineShow={false}
             />
             </div>
 
             </div>
             <div className="col-sm-1" style={{backgroundColor:'white'}}>
-            <DatePicker hintText="Start Date" mode="landscape"  floatingLabelText="Start Date"/>
+            <DatePicker hintText="Start Date" mode="landscape"  
+            onChange={this.handleStartDate.bind(this)}
+            floatingLabelText="Start Date"/>
             </div>
             <div className="col-sm-1" style={{backgroundColor:'white'}}>
-            <DatePicker hintText="Return Date" mode="landscape" floatingLabelText="Return Date"/>
+            <DatePicker hintText="Return Date" mode="landscape"
+            onChange={this.handleEndDate.bind(this)} 
+            floatingLabelText="Return Date"/>
             </div>
             <div className="col-sm-3">
             <div style={{backgroundColor:'white'}} onClick={()=>this.handlepopup()}>    
             <TextField
                  hintText="Travelers"
                  floatingLabelText="Travelers"
-                 underlineShow='false'
+                 underlineShow={false}
                  value={this.state.traveler+" Travelers, "+this.state.class}
+
             />
             </div>
             </div>
             <div className="col-sm-1" >
 
-            <button type="button" className="btn btn-deep-orange" style={{marginLeft:"-10%",height:'60px'}}>
+            <button type="button" className="btn btn-deep-orange" 
+            onClick={()=>this.handleSubmit()}
+            style={{marginLeft:"-10%",height:'60px'}}>
                  <i className="fa fa-arrow-right" aria-hidden="true"></i></button>
             </div>
            
@@ -151,6 +226,7 @@ class Flightsearch extends Component {
 
         </div>
          {this.displaypopup()}
+        
         </div>
         )
     }
