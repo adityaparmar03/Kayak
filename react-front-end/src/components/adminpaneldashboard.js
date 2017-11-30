24cd0b4f33980bf2f09dc0a49aadc0d47e28c12d
Aditya Parmar <parmar415@gmail.com>
2017-11-30 06:25:38 -0800

    costmatic changes

---
+++ react-front-end/src/components/adminpaneldashboard.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Link,withRouter} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class AdminPanelDashboard extends Component {
+
+
+    componentWillMount(){
+       
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Hello, This is Kayak!!</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default AdminPanelDashboard;
