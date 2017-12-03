58df24e429389e216e834a245b08eab6e8f62d68
Aditya Parmar <parmar415@gmail.com>
2017-12-03 01:44:01 -0800

    booking data gathered

--- react-front-end/src/components/hotelbooking.js
+++ react-front-end/src/components/hotelbooking.js
@@ -37,12 +37,26 @@ class HotelBooking extends Component {
 
       })
 
-      console.log('payload',payload);
+    
 
     }
 
     handlePay(){
       const payload = JSON.parse(localStorage.getItem("hotelbooking"));
+     
+      var travellerinfo = {
+          "firstname":this.refs.firstname.value,
+          "lastname":this.refs.lastname.value,
+          "email":this.refs.email.value,
+          "phoneno":this.refs.phoneno.value,
+          "address":this.refs.address.value,
+          "zipcode":this.refs.zipcode.value
+      }
+      var credit_card = {
+            "card_number": this.refs.creditcardno.value,
+            "valid_till":this.refs.expirydate.value,
+            "cvv":this.refs.cvv.value
+      }
       API.bookHotel(payload)
           .then((res) => {
               console.log(res);
@@ -114,7 +128,8 @@ class HotelBooking extends Component {
                                             <div className="col-sm-6">
                                                 <div className="md-form">
                                                     <i className="fa fa-user prefix"></i>
-                                                    <input type="text" id="firstname" className="form-control"/>
+                                                    <input type="text" id="firstname"
+                                                    ref="firstname" className="form-control"/>
                                                     <label htmlFor="firstname">Firstname</label>
                                                 </div>
 
@@ -122,7 +137,8 @@ class HotelBooking extends Component {
                                             <div className="col-sm-6">
                                                 <div className="md-form">
                                                     <i className="fa fa-user prefix"></i>
-                                                    <input type="text" id="lastname" className="form-control"/>
+                                                    <input type="text" id="lastname"
+                                                     ref="lastname" className="form-control"/>
                                                     <label htmlFor="lastname">Lastname</label>
                                                 </div>
 
@@ -132,7 +148,8 @@ class HotelBooking extends Component {
                                         <div className="col-sm-6">
                                         <div className="md-form">
                                         <i className="fa fa-envelope prefix"></i>
-                                        <input type="text" id="email" className="form-control"/>
+                                        <input type="text" id="email" 
+                                        ref="email" className="form-control"/>
                                         <label htmlFor="email">Email</label>
                                         </div>
 
@@ -142,7 +159,8 @@ class HotelBooking extends Component {
                                         <div className="md-form">
                                         <i className="fa fa-phone prefix"></i>
 
-                                        <input type="text" id="phone" className="form-control"/>
+                                        <input type="text" id="phone" 
+                                        ref="phoneno" className="form-control"/>
                                         <label htmlFor="phone">Phone Number</label>
 
                                         </div>
@@ -154,7 +172,8 @@ class HotelBooking extends Component {
                                         <div className="md-form">
                                         <i className="fa fa-map-marker prefix"></i>
 
-                                        <input type="text" id="address" className="form-control"/>
+                                        <input type="text" id="address" 
+                                        ref="address" className="form-control"/>
                                         <label htmlFor="address">Address</label>
 
                                         </div>
@@ -164,7 +183,7 @@ class HotelBooking extends Component {
                                         <div className="md-form">
                                         <i className="fa fa-location-arrow prefix"></i>
 
-                                        <input type="text" id="zipcode" className="form-control"/>
+                                        <input type="text" id="zipcode" ref="zipcode" className="form-control"/>
                                         <label htmlFor="zipcode">Zip Code</label>
 
                                         </div>
@@ -185,7 +204,9 @@ class HotelBooking extends Component {
                                 <div className="col-sm-4">
                                     <div className="md-form form-group">
                                     <i className="fa fa-credit-card-alt prefix"></i>
-                                    <input type="text" id="creditcardno" className="form-control validate" maxLength='16'/>
+                                    <input type="text" id="creditcardno" 
+                                    ref="creditcardno"
+                                    className="form-control validate" maxLength='16'/>
                                     <label htmlFor="creditcardno">Credit Card No</label>
                                     </div>
 
@@ -194,14 +215,16 @@ class HotelBooking extends Component {
                                     <label>Expiry Date :  </label>
                                     <div className="md-form form-group">
 
-                                        <input type="month" id="form92" className="form-control validate"/>
+                                        <input type="month" id="form92" 
+                                        ref="expirydate" className="form-control validate"/>
 
                                     </div>
 
                                 </div>
                                 <div className="col-sm-4">
                                     <div className="md-form form-group">
-                                    <input type="text" id="cvv" className="form-control validate" maxLength='3'/>
+                                    <input type="text" id="cvv" 
+                                    ref="cvv" className="form-control validate" maxLength='3'/>
                                     <label htmlFor="cvv">CVV</label>
                                     </div>
 
