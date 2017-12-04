48a686d2324685738ce32688ef87e69f26f2a5f6
siddharth <siddharth.suthar053@gmail.com>
2017-12-03 23:13:06 -0800

    Admin.js changes

--- kafka-back-end/services/admin.js
+++ kafka-back-end/services/admin.js
@@ -165,7 +165,7 @@ function getBills(msg,callback) {
 
 function getusers(msg,callback) {
     var res = {};
-    var getQuery = "select first_name,last_name,email,phone,street_address,zip_code from user ; ";
+    var getQuery = "select first_name,last_name,email,phone,street_address,zip_code from user where user_role ='USER' ; ";
     mysql.fetchData(function (err,results) {
         if(err){
             res.code = 401;
